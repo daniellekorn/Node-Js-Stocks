685a9511b527e097b1779f9f11a29c11206b16ec
Danielle Korn <daniella.korn@gmail.com>
2020-04-01 20:03:21 +0300

    delete endpoint added

--- index.js
+++ index.js
@@ -56,8 +56,22 @@ app.get("/search-history", (req, res) => {
 		.sort({ date: -1 })
 		.toArray()
 		.then(searches => {
+			// res.render("search-history.handlebars", { history: searches });
+			res.json({ history: searches });
+		});
+});
+
+app.get("/search-history/:id", (req, res) => {
+	const ObjectId = require("mongodb").ObjectID;
+	const collection = client.db("itc-stocks").collection("searches");
+	const id = req.params.id;
+	collection.deleteOne({ _id: ObjectId(`${id}`) });
+	collection
+		.find()
+		.sort({ date: -1 })
+		.toArray()
+		.then(searches => {
 			res.render("search-history.handlebars", { history: searches });
-			// res.json({ history: searches });
 		});
 });
 
